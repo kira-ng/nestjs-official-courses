4e2ffe27e2c1e5cd40f53f114b103a52c8deca7f
KiraNg <tuanna1906@gmail.com>
2022-08-25 17:30:33 +0900

    iluvcoffee | Env Configuration

--- iluvcoffee/src/app.module.ts
+++ iluvcoffee/src/app.module.ts
@@ -4,26 +4,19 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingsModule } from './coffee-ratings/coffee-ratings.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
-      // type: 'postgres',
-      // host: 'localhost',
-      // port: 5432,
-      // username: 'postgres',
-      // password: 'asdf1234',
-      // database: 'postgres',
-      // autoLoadEntities: true,
-      // synchronize: true,
-
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'asdf1234',
-      database: 'iluvcoffee',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: false,
     }),
